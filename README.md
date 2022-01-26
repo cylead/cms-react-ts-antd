f278db28a4e4d5348ad601abf62c43760152ab4e
cylead <yangchengjxja@sina.com>
2022-01-26 19:58:14 +0800

    first commit

---
+++ README.md
@@ -0,0 +1,5 @@
+progress
+
+1. clean the src 👌
+2. add App.tsx and index.tsx 👌
+3. git 提交
---
+++ src/index.tsx
@@ -0,0 +1,5 @@
+import ReactDom from "react-dom";
+import App from "./App";
+import "antd/dist/antd.css";
+
+ReactDom.render(<App />, document.getElementById("root"));
